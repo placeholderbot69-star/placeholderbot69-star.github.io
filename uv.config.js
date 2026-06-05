c9520fffe3ee50fb4ad7cebac3a5349dee6c163e
placeholderbot69-star <placeholderbot.69@gmail.com>
2026-06-05 13:06:00 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,11 +1,10 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
-    bare: 'https://your-bare-server.onrender.com/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv.handler.js',
-    bundle: '/uv.bundle.js',
-    config: '/uv.config.js',
-    sw: '/uv.sw.js'
+  prefix: '/service/',
+  bare: 'https://your-render-url.onrender.com/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/uv.handler.js',
+  bundle: '/uv.bundle.js',
+  config: '/uv.config.js',
+  sw: '/uv.sw.js'
 };
